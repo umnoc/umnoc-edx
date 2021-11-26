fdc3c443227417c775fe99518796aec84e7995d4
MasterGowen <mastergowen@gmail.com>
2021-11-26 10:58:41 +0500

    api programs get

--- umnoc/api.py
+++ umnoc/api.py
@@ -1,8 +1,27 @@
+from datetime import date
+from uuid import uuid4
+
 from ninja import NinjaAPI
+from ninja import Schema
+
+from .core.models import Program, Project
 
 api = NinjaAPI()
 
 
-@api.get("/hello")
-def hello(request):
-    return "Hello world"
+class ProgramOut(Schema):
+    uuid: uuid4
+    title: str
+    short_name: str
+    slug: str
+    description: str
+    logo: str
+    number_of_hours: int
+    edu_start_date: date = None
+    edu_end_date: date = None
+
+
+@api.get("/programs")
+def programs(request, response=List[ProgramOut]):
+    qs = Program.objects.filter(active=True, status='published')
+    return qs
