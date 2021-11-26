dba54b6bf2bd86e2de4452dcba0531a7626e1439
MasterGowen <mastergowen@gmail.com>
2021-11-26 06:37:21 +0500

    up

--- umnoc/models.py
+++ umnoc/models.py
@@ -2,7 +2,7 @@
 Database models for umnoc.
 """
 # from django.db import models
-from umnoc.core.models import (Organization, Direction, Project, TextBlock)
-from umnoc.courses.models import (Course, ProgramCourse, OrganizationCourse)
-from umnoc.learners.models import (ProgramEnrollment)
-from umnoc.profiles.models import (Profile, Reflection, Question, Answer)
+from .core.models import (Organization, Direction, Project, TextBlock)
+from .courses.models import (Course, ProgramCourse, OrganizationCourse)
+from .learners.models import (ProgramEnrollment)
+from .profiles.models import (Profile, Reflection, Question, Answer)
