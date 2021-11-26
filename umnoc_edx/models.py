8a3ef6abb0f5513a29d0fb395968fa682a47b837
MasterGowen <mastergowen@gmail.com>
2021-11-26 05:48:10 +0500

    initial

--- umnoc_edx/models.py
+++ umnoc_edx/models.py
@@ -2,7 +2,7 @@
 Database models for umnoc_edx.
 """
 # from django.db import models
-from core.models import (Organization, Direction, Project, Project, TextBlock)
+from core.models import (Organization, Direction, Project, TextBlock)
 from courses.models import (Course, ProgramCourse, OrganizationCourse)
 from learners.models import (ProgramEnrollment)
 from profiles.models import (Profile, Reflection, Question, Answer)
