67164cefb71cd97e1c84c04c9b401944cee184b7
MasterGowen <mastergowen@gmail.com>
2021-11-26 06:04:57 +0500

    absolute imports

--- umnoc_edx/models.py
+++ umnoc_edx/models.py
@@ -2,7 +2,7 @@
 Database models for umnoc_edx.
 """
 # from django.db import models
-from core.models import (Organization, Direction, Project, TextBlock)
-from courses.models import (Course, ProgramCourse, OrganizationCourse)
-from learners.models import (ProgramEnrollment)
-from profiles.models import (Profile, Reflection, Question, Answer)
+from umnoc_edx.core.models import (Organization, Direction, Project, TextBlock)
+from umnoc_edx.courses.models import (Course, ProgramCourse, OrganizationCourse)
+from umnoc_edx.learners.models import (ProgramEnrollment)
+from umnoc_edx.profiles.models import (Profile, Reflection, Question, Answer)
