ca5f8c374b008384a6404cfe2ae67e120d8412fd
MasterGowen <mastergowen@gmail.com>
2021-12-20 21:21:48 +0500

    course admin fieldsets#2

--- umnoc/admin.py
+++ umnoc/admin.py
@@ -89,14 +89,14 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('display_name', 'course_id', 'start_date', 'end_date', 'course_program', 'status')
     fieldsets = (
         (None, {
-            'fields': (('course_overview', 'status', 'is_removed', 'published_at'),)
+            'fields': (('course_overview', 'status', 'is_removed'), 'published_at')
         }),
-        ('Numeric fields', {
-            'classes': ('collapse',),
+        ('Численная информация', {
+            'classes': ('wide',),
             'fields': (('min_duration', 'max_duration'), 'labor', 'lectures_count')
         }),
-        ('Advanced options', {
-            'classes': ('collapse',),
+        ('Текстовая информация', {
+            'classes': ('wide',),
             'fields': ('target', 'description', 'course_program', 'prerequisites', 'format'),
         }),
     )
