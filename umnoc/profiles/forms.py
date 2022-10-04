1c5c360a12c2e5af77cdd962eb417d5d32e84c58
MasterGowen <mastergowen@gmail.com>
2022-10-04 08:33:36 +0500

    test registration form extension

--- umnoc/profiles/forms.py
+++ umnoc/profiles/forms.py
@@ -1,8 +1,21 @@
 from django.forms import ModelForm
+
 from .models import Profile1
 
 
 class ProfileForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['SNILS'].error_messages = {
+            "required": u"Введите СНИЛС",
+            "invalid": u"Некорректно",
+        }
+        self.fields['specialty'].error_messages = {
+            "required": u"Введите специальность (направление подготовки)",
+            "invalid": u"Некорректно",
+        }
+
     class Meta:
         model = Profile1
         fields = ['SNILS', 'specialty']
