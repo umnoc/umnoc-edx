f76bbc2cfb5c6557a6237d5496f7957f5bd8e61a
MasterGowen <mastergowen@gmail.com>
2022-12-02 08:22:35 +0500

    ~external courses

--- umnoc/core/views.py
+++ umnoc/core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import View
-
+from django.http import HttpResponse
 from .models import ExternalPlatform
-from django.contrib.admin.views.decorators import staff_member_required
+
 
 class GetExternalCourses(View):
     """
@@ -15,7 +15,7 @@ class GetExternalCourses(View):
         context['sources'] = sources
         return render(request, template_name='umnoc_edx/staff/external_courses.html', context=context)
 
-# @staff_member_required
-# def assimilate(request):
-#     if request.method.upper() == "POST":
+    def post(self, request, *args, **kwargs):
+        external_course_id = request.POST("external_course_id")
+        return HttpResponse(external_course_id)
 
