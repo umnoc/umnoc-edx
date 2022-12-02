3e48017fb80f179c3df0c05e3110901491a8118c
MasterGowen <mastergowen@gmail.com>
2022-12-02 15:31:34 +0500

    urfu profile schema

--- umnoc/schema.py
+++ umnoc/schema.py
@@ -21,10 +21,10 @@ class RoleSchema(Schema):
 
 
 class UrFUProfileSchema(ModelSchema):
-    roles: List[RoleSchema]
+    list_roles: Any
 
     @staticmethod
-    def resolve_roles(obj):
+    def resolve_list_roles(obj):
         return obj.list_roles
 
     # @staticmethod
