3115b3d930ec23cfc15aad26617d04923c7bc867
MasterGowen <mastergowen@gmail.com>
2023-10-26 15:22:34 +0500

    display_name_f

--- umnoc/api.py
+++ umnoc/api.py
@@ -49,7 +49,7 @@ api = NinjaAPI(renderer=ORJSONRenderer(), csrf=True)
 
 
 class CourseFilterSchema(FilterSchema):
-    search: Optional[str] = Field(q=['display_name__icontains'])
+    search: Optional[str] = Field(q=['display_name_f__icontains'])
 
 
 @api.get("/me", auth=django_auth, response=UserProfileSchema)
