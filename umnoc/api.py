2b3bb52777defe3e90beacd2ad84b4e346ce1e46
MasterGowen <mastergowen@gmail.com>
2023-11-14 23:07:53 +0500

    urls_namespace='umnoc-api'

--- umnoc/api.py
+++ umnoc/api.py
@@ -48,7 +48,7 @@ class ORJSONRenderer(BaseRenderer):
         return orjson.dumps(data, default=self.default)
 
 
-api = NinjaAPI(renderer=ORJSONRenderer())
+api = NinjaAPI(renderer=ORJSONRenderer(), urls_namespace='umnoc-api')
 
 
 @api.exception_handler(AuthenticationError)
